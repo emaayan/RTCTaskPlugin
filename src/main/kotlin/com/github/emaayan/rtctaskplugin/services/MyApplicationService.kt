31aa1e8b3bdc40ad8781f4de3a2e94d6f95591fd
GitHub Action <action@github.com>
2021-09-20 14:46:04 +0000

    Template cleanup

--- src/main/kotlin/com/github/emaayan/rtctaskplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/emaayan/rtctaskplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.emaayan.rtctaskplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.emaayan.rtctaskplugin.MyBundle
 
 class MyApplicationService {
 
--- src/test/kotlin/com/github/emaayan/rtctaskplugin/MyPluginTest.kt
+++ src/test/kotlin/com/github/emaayan/rtctaskplugin/MyPluginTest.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.emaayan.rtctaskplugin
 
 import com.intellij.ide.highlighter.XmlFileType
 import com.intellij.psi.xml.XmlFile
